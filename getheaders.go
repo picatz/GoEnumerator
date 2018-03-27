d896022436e395becac00d0987a3a160e94423a2
rek2 <cfernandez@protonmail.ch>
2018-03-26 22:19:35 -0700

    get headers and output to stdout

--- getheaders.go
+++ getheaders.go
@@ -1,3 +1,21 @@
 package main
 
-func getHeaders(url string) {}
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func getHeaders(url string) {
+
+	// Perform HTTP HEAD
+	response, err := http.Head(url)
+	if err != nil {
+		log.Fatal("Error fetching URL. ", err)
+	}
+
+	// Print out each header key and value pair
+	for key, value := range response.Header {
+		fmt.Printf("%s: %s\n", key, value[0])
+	}
+}
