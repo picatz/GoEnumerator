ab65d22194c1265fc30aa3d8158689a71ee5ff60
rek2 <cfernandez@protonmail.ch>
2018-04-05 22:12:30 -0700

    changed a bit the algorinmt

--- searchcve.go
+++ searchcve.go
@@ -9,27 +9,29 @@ import (
 func searchCVE(Banners map[int]string, CVE map[int]interface{}) {
 
 	//fmt.Println(CVE[2013])
+	//var word string
 
 	for _, banner := range Banners {
 		re := regexp.MustCompile("[-,/,(,),_]")
 		NewBanner := re.ReplaceAllString(strings.TrimSpace(banner), " ")
 		BannerSlice := strings.Split(NewBanner, " ")
-		for _, word := range BannerSlice {
-			fmt.Println(word)
-			for _, Data := range CVE {
-				for _, cveitems := range Data.(CVEParse).CVEItems {
-					for _, Vendor := range cveitems.Cve.Affects.Vendor.VendorData {
-						for _, vname := range Vendor.Product.ProductData {
+		for _, Data := range CVE {
+			for _, cveitems := range Data.(CVEParse).CVEItems {
+				for _, Vendor := range cveitems.Cve.Affects.Vendor.VendorData {
+					for _, vname := range Vendor.Product.ProductData {
 
-							fmt.Println(vname.ProductName)
-							//}
-							//if Data.(CVEParse).CVEItems[item].Cve.Affects.Vendor[word] {
-							//fmt.Println(Vendor)
+						if strings.Contains(vname.ProductName, BannerSlice[0]) {
+							fmt.Println(BannerSlice[0])
+							fmt.Println("Match " + vname.ProductName)
 						}
+						//}
+						//if Data.(CVEParse).CVEItems[item].Cve.Affects.Vendor[word] {
+						//fmt.Println(Vendor)
 					}
 				}
 			}
 		}
+
 	}
 
 }
